f0c0e42f1c76c5b18d14a5f9eb73aee17320bb1c
sofi <bedlusofi@gmail.com>
2023-02-10 18:15:54 +0000

    State class added

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Define the State class """
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ represents State instances """
+
+    name = ''
+    
+    def __init__(self, *args, **kwargs):
+        """ initialize the the State instances with the Base class """
+        super().__init__(*args, **kwargs)
